369843d021dcf0c3c3b37509da10497eefe33216
Shashank Dhakate <shashankdkte@gmail.com>
2023-11-22 19:43:12 +0530

    Added Navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
+import { IoIosColorPalette } from "react-icons/io";
 import './navbar.css'
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
+
 
 const Navbar = () => {
+  console.log(data)
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className="nav__logo">
+          <img src={Logo} alt="Logo"  className="nav__logo"/>
+        </a>
+         <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+         <button id='theme__icon'><IoIosColorPalette/></button>
+      </div>
+    </nav>
   )
 }
 
