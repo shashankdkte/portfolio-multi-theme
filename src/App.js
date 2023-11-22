247bfb7fff50e4ed60e733adce64e6237f1fd337
Shashank Dhakate <shashankdkte@gmail.com>
2023-11-22 17:22:42 +0530

    Initial Project Layout with Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './sections/navbar/Navbar';
+import Header from './sections/header/Header';
+import About from './sections/about/About';
+import Services from './sections/services/Services';
+import Portfolio from './sections/portfolio/Portfolio';
+import Testimonials from './sections/testimonials/Testimonials';
+import FAQs from './sections/faqs/FAQs';
+import Contact from './sections/contact/Contact';
+import Footer from './sections/footer/Footer';
+import FloatingNav from './sections/floating-nav/FloatingNav';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+        <Navbar/>
+        <Header/>
+        <About/>
+        <Services/>
+        <Portfolio/>
+        <Testimonials/>
+        <FAQs/>
+        <Contact/>
+        <Footer/>
+        <FloatingNav/>
+    </main>
+  )
 }
 
-export default App;
+export default App
